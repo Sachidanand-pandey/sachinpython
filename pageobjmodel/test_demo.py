205a84bf81007d1224a46d43b5816bbc11e6076c
Sachidanand-pandey <sachinbst41@gmail.com>
2022-11-21 11:19:49 +0530

    intial commit

--- pageobjmodel/test_demo.py
+++ pageobjmodel/test_demo.py
@@ -23,7 +23,7 @@ class Testhrm():
     def test_homepagetitle(self,setup):
         self.driver.get('http://tutorialsninja.com/demo/')
         time.sleep(4)
-        actual_tittle=self.driver.title
+        actual_tittle = self.driver.title
         if actual_tittle == 'Your Story':
             assert True
         else:
@@ -35,35 +35,35 @@ class Testhrm():
     @allure.story('second test case')
     @allure.description('in this we add the product into cart and verify the price')
     @allure.step('go to the page and add the product to the cart and verify the price of the product are same')
-    def test_login(self,setup):
+    def test_login(self, setup):
         self.driver.get('http://tutorialsninja.com/demo/')
         time.sleep(4)
-        s=self.driver.find_element(By.XPATH,'//*[@id="search"]/input')
+        s = self.driver.find_element(By.XPATH, '//*[@id="search"]/input')
         s.send_keys('Macbook')
         s.send_keys(Keys.ENTER)
         time.sleep(5)
-        self.driver.find_element(By.XPATH,'//*[@id="content"]/div[3]/div/div/div[2]/div[2]/button').click()
+        self.driver.find_element(By.XPATH, '//*[@id="content"]/div[3]/div/div/div[2]/div[2]/button').click()
         time.sleep(5)
-        self.driver.find_element(By.XPATH,'//div[@id="cart"]/button').click()
-        a=self.driver.find_element(By.XPATH,'//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[1]/td[2]')
-        d=(a.text)
-        e=d.split('$')
+        self.driver.find_element(By.XPATH, '//div[@id="cart"]/button').click()
+        a = self.driver.find_element(By.XPATH, '//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[1]/td[2]')
+        d = a.text
+        e = d.split('$')
         print(e[1])
-        b=self.driver.find_element(By.XPATH,'//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[2]/td[2]')
-        f=(b.text)
-        g=f.split('$')
+        b = self.driver.find_element(By.XPATH, '//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[2]/td[2]')
+        f = b.text
+        g = f.split('$')
         print(g[1])
-        c=self.driver.find_element(By.XPATH,'//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[3]/td[2]')
-        h=(c.text)
-        i=h.split('$')
+        c = self.driver.find_element(By.XPATH, '//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[3]/td[2]')
+        h = c.text
+        i = h.split('$')
         print(i[1])
-        result=float(float(e[1])+float(g[1])+float(i[1]))
+        result = float(float(e[1])+float(g[1])+float(i[1]))
         print(result)
-        v=self.driver.find_element(By.XPATH,'//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[4]/td[2]')
-        res=(v.text)
-        ver=res.split('$')
-        resu=float(ver[1])
-        if result==resu:
+        v = self.driver.find_element(By.XPATH, '//*[@id="cart"]/ul/li[2]/div/table/tbody/tr[4]/td[2]')
+        res = v.text
+        ver = res.split('$')
+        resu = float(ver[1])
+        if result == resu:
             print('test case pass')
             assert True
             allure.attach(self.driver.get_screenshot_as_png(), name="verifyprice", attachment_type=AttachmentType.PNG)
@@ -78,22 +78,22 @@ class Testhrm():
     def test_verify(self, setup):
         self.driver.get('http://tutorialsninja.com/demo/')
         time.sleep(2)
-        a=self.driver.find_element(By.XPATH,'//nav[@id="menu"]/div[2]/ul/li[2]/a')
+        a = self.driver.find_element(By.XPATH, '//nav[@id="menu"]/div[2]/ul/li[2]/a')
         a.click()
         time.sleep(4)
-        b=self.driver.find_element(By.XPATH,"//a[text()='Show All Laptops & Notebooks']")
+        b = self.driver.find_element(By.XPATH, "//a[text()='Show All Laptops & Notebooks']")
         b.click()
         time.sleep(4)
-        c=self.driver.find_element(By.XPATH,'//i[@class="fa fa-th-list"]')
+        c = self.driver.find_element(By.XPATH, '//i[@class="fa fa-th-list"]')
         c.click()
         time.sleep(4)
-        d=self.driver.find_element(By.XPATH,'//*[@id="content"]/div[4]/div[3]/div/div[2]/div[2]/button[1]')
+        d = self.driver.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[3]/div/div[2]/div[2]/button[1]')
         d.click()
         time.sleep(3)
-        e=self.driver.find_element(By.XPATH,'//span[@id="cart-total"]')
+        e = self.driver.find_element(By.XPATH, '//span[@id="cart-total"]')
         e.click()
-        f=self.driver.find_element(By.XPATH,'//td[@class="text-left"]/a')
-        g=f.text
+        f = self.driver.find_element(By.XPATH, '//td[@class="text-left"]/a')
+        g = f.text
         print(g)
         if g == 'MacBook Air':
             assert True
@@ -109,21 +109,21 @@ class Testhrm():
     def test_tablets(self,setup):
         self.driver.get('http://tutorialsninja.com/demo/')
         time.sleep(4)
-        a=self.driver.find_element(By.XPATH,"//a[text()='Tablets']")
+        a = self.driver.find_element(By.XPATH,"//a[text()='Tablets']")
         a.click()
         time.sleep(4)
-        b=self.driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div/div/div[2]/div[2]/button[1]')
+        b = self.driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div/div/div[2]/div[2]/button[1]')
         b.click()
         time.sleep(3)
-        c=self.driver.find_element(By.XPATH,'//span[@id="cart-total"]')
+        c = self.driver.find_element(By.XPATH,'//span[@id="cart-total"]')
         c.click()
         time.sleep(3)
-        d=self.driver.find_element(By.XPATH,'//*[@id="cart"]/ul/li[1]/table/tbody/tr/td[2]/a')
-        e=d.text
+        d = self.driver.find_element(By.XPATH,'//*[@id="cart"]/ul/li[1]/table/tbody/tr/td[2]/a')
+        e = d.text
         print(e)
-        if e=='Samsung Galaxy Tab 10.1':
+        if e == 'Samsung Galaxy Tab 10.1':
             assert True
+            allure.attach(self.driver.get_screenshot_as_png(), name='verifytab',attachment_type=AttachmentType.PNG)
         else:
             assert False
-
 
