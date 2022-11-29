db3f8434f1aef142c01e4bfb2d9569be5c7bd149
Sachidanand-pandey <sachinbst41@gmail.com>
2022-11-29 12:39:30 +0530

    intial commit

--- extractemail.py
+++ extractemail.py
@@ -1,69 +1,36 @@
-import imaplib,email
 import time
-import datetime as dt
-from os import path
-from datetime import timezone
-
-user="sachinbst41@gmail.com"
-password="utkarsh@123"
-imap_url ="imap.gmail.com"
-
-def get_body(msg):
-    if msg.is_multipart():
-        return get_body(msg.get_payload(0))
-    else:
-        return msg.get_payload(None,True)
-date_time = dt.datetime.now()
-f=open("gmail_time.txt","r")
-a=f.read()
-f.close()
-
-a= dt.datetime.strptime(a,'%a,%d,%b,%Y,%H:%M:%S %Z')
-
-date_time.replace(tzinfo=timezone.utc)
-
-date_time=date_time.replace(tzinfo=timezone.utc).isoformat()
-
-date_time=dt.datetime.strptime(date_time,'%Y-%m-%dT%H:%M:%S.%f%z')
-
-lastDayDateTime = date_time -a
-
-con = imaplib.IMAP4_SSL(imap_url)
-con.login(user,password)
-con.select('INBOX')
-dates = (dt.datetime.now()-lastDayDateTime).strptime('%d-%b-%Y')
-resp,items = con.uid('search', None,'(SENTSINCE {date})'.format(date=dates))
-a = items
-b=a[0].decode('utf-8')
-b=b.split(" ")
-f1= open("gmail_mails.txt","w+")
-
-for value in b:
-    late=int(value)-801
-    late =str(late)
-    result,data =con.fetch(late,'(RFC822)')
-    raw = email.message_from_bytes(data[0][1])
-    print(raw['Date'])
-    print(raw['From'])
-    print(raw['Body'])
-    f1.write('Date = '+ raw['Date'] + ' ')
-    f1.write('From = '+ raw['From'] + ' ')
-    f1.write('Subject = '+ raw['Subject'] + ' ')
-    f1.write('Body = '+ str(get_body(raw)) + ' ')
-    f1.write(' ')
-f1.close()
-f2= open("gmail_time.txt","w+")
-f2.write(raw['Date'])
-f2.close()
+from allure_commons.types import AttachmentType
+import allure
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+class Testemail():
+    @pytest.fixture()
+    def setup(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.maximize_window()
+        yield
+        self.driver.close()
+    def test_search(self,setup):
+        self.driver.get('https://www.google.com/')
+        a=self.driver.find_element(By.XPATH,'//input[@class="gLFyf"]')
+        a.send_keys('www.gmail.com')
+        a.send_keys(Keys.ENTER)
+        self.driver.find_element(By.XPATH,"//h3[text()='Google Accounts: Sign in']").click()
+        b=self.driver.find_element(By.XPATH,'//input[@id="identifierId"]')
+        b.send_keys('sachinbst41@gmail.com')
+        time.sleep(3)
+        self.driver.find_element(By.XPATH,"//span[text()='Next']").click()
+        time.sleep(3)
+        c=self.driver.find_element(By.XPATH,'//input[@type="password"]')
+        c.send_keys('utkarsh@123')
+        c.send_keys(Keys.ENTER)
+        time.sleep(4)
 
 
 
-
-
-
-
-
-
-
-
+        time.sleep(3)
 
